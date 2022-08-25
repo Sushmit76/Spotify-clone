bf269b549a253713c3a4e7ebe333bca295336c2d
sushmit srivastava <sushmits76btechcse2024@kccitm.edu.in>
2022-08-26 03:03:41 +0530

    ongoing

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ console.log("Hello");
 
 let songIndex = 0;
 let audioVolume = 0;
-let audioElement=new Audio('Kun Faya Kun.mp3');
-let masterPlay=document.getElementById('Play');
+let audioElement=new Audio('/songs/Kun Faya Kun.mp3');
+let Play=document.getElementById('Play');
 let durationbar=document.getElementById('durationbar');
 let volume=document.getElementById('volume');
 let sound=document.getElementById('sound');
-let masterSongName = document.getElementById('masterSongName');
-let songlists = Array.from(document.getElementsByClassName('songlists'));
+// let masterSongName = document.getElementById('masterSongName');
+// let songlists = Array.from(document.getElementsByClassName('songlists'));
 
 let songs = [
     {songName: "Kun Faya Kun", filePath: "/Spotify clone/songs/Kun Faya Kun.mp3",coverPath: "/spotify clone/cover1.jpg"},
@@ -19,28 +19,30 @@ let songs = [
 
 
 ]
-songlists.forEach((element, i)=>{ 
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
-})
+// songlists.forEach((element, i)=>{ 
+//     element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+//     element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+// })
 
-Play.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
+// for play and pause
+Play.addEventListener('click' , ()=>{
+    if(Play.classList.contains("fa-circle-play")){
         audioElement.play();
         Play.classList.remove('fa-circle-play');
         Play.classList.add('fa-circle-pause');
+                //  console.log('play button');
 
     }
     else{
         audioElement.pause();
+        // console.log('pause button');
         Play.classList.remove('fa-circle-pause');
         Play.classList.add('fa-circle-play');
     }
-    
 
 })
 
-
+// for duration bar
 audioElement.addEventListener('timeupdate', ()=>{
     progress=parseInt((audioElement.currentTime/audioElement.duration) * 100);
     durationbar.value=progress;
@@ -51,6 +53,7 @@ durationbar.addEventListener('change', ()=>{
 
 })
 
+// for sound bar
 sound.addEventListener('click' , ()=>{
     if(audioVolume<=0){
         
@@ -67,50 +70,3 @@ sound.addEventListener('click' , ()=>{
 // volume.addEventListener('change' , ()=>{
     
 // })
-
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click', (e)=>{ 
-        makeAllPlays();
-        songIndex = parseInt(e.target.id);
-        e.target.classList.remove('fa-play-circle');
-        e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-    })
-})
-
-document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>=9){
-        songIndex = 0
-    }
-    else{
-        songIndex += 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.add('fa-pause-circle');
-
-})
-
-document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
-        songIndex = 0
-    }
-    else{
-        songIndex -= 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.add('fa-pause-circle');
-})
