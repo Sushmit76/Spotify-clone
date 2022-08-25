cbadd851f345cbcbaeb1e9b35beac967cb9088e9
sushmit srivastava <sushmits76btechcse2024@kccitm.edu.in>
2022-08-26 04:30:56 +0530

    musicupdated

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let audioElement=new Audio('/songs/1.mp3');
 let Play=document.getElementById('Play');
 let Previous=document.getElementById('Previous');
 let Next=document.getElementById('Next');
+let Shuff=document.getElementById('Shuff');
+let Rept=document.getElementById('Rept');
 let durationbar=document.getElementById('durationbar');
 let volume=document.getElementById('volume');
 let sound=document.getElementById('sound');
@@ -13,7 +15,7 @@ let sound=document.getElementById('sound');
 // let songlists = Array.from(document.getElementsByClassName('songlists'));
 
 let songs = [
-    {songName: "Kun Faya Kun", filePath: "/Spotify clone/songs/4.mp3",coverPath: "/spotify clone/cover1.jpg"},
+    {songName: "Kun Faya Kun", filePath: "/Spotify clone/songs/4.mp3",coverPath: "/Spotify clone/cover1.jpg"},
     {songName: "Haan Main Galat", filePath: "/Spotify clone/songs/2.mp3",coverPath: "/spotify clone/cover2.webp"},
     {songName: "Humraah", filePath: "/Spotify clone/songs/3.pm3",coverPath: "/spotify clone/cover3.webp"},
     {songName: "Yaar Mod Do", filePath: "/Spotify clone/songs/5.mp3",coverPath: "/spotify clone/cover5.jpg"},
@@ -21,6 +23,22 @@ let songs = [
 
 
 ]
+// after ending of song
+audioElement.addEventListener('ended', ()=> {
+    durationbar.value=0;
+    if(songIndex>=5){
+        songIndex=1;
+
+    }
+    else{
+        songIndex+=1;
+        
+    }
+    audioElement.src=`/songs/${songIndex}.mp3`;
+    audioElement.play();
+
+
+})
 
 // for play and pause
 Play.addEventListener('click' , ()=>{
@@ -64,9 +82,36 @@ Previous.addEventListener('click' , ()=>{
         
     }
     audioElement.src=`/songs/${songIndex}.mp3`;
+    
     audioElement.play();
 })
+// for shuffle
+Shuff.addEventListener('click' ,()=>{
+    let rndInt = Math.floor(Math.random() * 5) + 1;
+    audioElement.src=`/songs/${rndInt}.mp3`;
+    audioElement.play();
+    // console.log(rndInt);
+})
+// for repeat
+Rept.addEventListener('click' ,()=>{
+    // audioElement.
+    if (typeof myAudio.loop == 'boolean')
+    {
+        myAudio.loop = true;
+    }
+    else
+    {
+        myAudio.addEventListener('ended', function() {
+            this.currentTime = 0;
+            this.play();
+        }, false);
+    }
+    myAudio.play();
 
+
+
+
+})
 // for duration bar
 audioElement.addEventListener('timeupdate', ()=>{
     progress=parseInt((audioElement.currentTime/audioElement.duration) * 100);
